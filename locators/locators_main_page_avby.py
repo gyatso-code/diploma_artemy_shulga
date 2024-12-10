bb85400ffefa1eeea8a39279b286683f0bacce7a
gyatso-code <doyzz12@hotmail.com>
2024-12-10 16:53:46 +0300

    my diploma

--- locators/locators_main_page_avby.py
+++ locators/locators_main_page_avby.py
@@ -13,39 +13,175 @@ class MainPage(WebPage):
 
         super().__init__(web_driver, url)
 
-    # btn_menu_header_logo = WebElement(xpath='//div[@class= "header__logo"]')
+    # Объявления
     btn_menu_header_announcement = WebElement(xpath='(//a[@class="nav__link"])[1]')
+
+    # Сервисы
     btn_menu_header_services = WebElement(xpath='(//a[@class="nav__link"])[2]')
+
+    # Журнал
     btn_menu_header_magazine = WebElement(xpath='(//a[@class="nav__link"])[3]')
+
+    # Знания
     btn_menu_header_knowledge = WebElement(xpath='(//a[@class="nav__link"])[4]')
+
+    # Услуги
     btn_menu_header_attendance = WebElement(xpath='(//a[@class="nav__link"])[5]')
+
+    # Проверка VIN
     btn_menu_header_vin_check = WebElement(xpath='(//a[@class="nav__link"])[6]')
+
+    # Войти
     btn_menu_header_login = WebElement(xpath='(//a[@class="nav__link"])[7]')
-    # btn_menu_header_login_two = WebElement(xpath='(//div[@class="drawer__slide-body"])[1]')
-    # btn_menu_header_place_an_ad = WebElement(xpath='//button[@class="button button--primary button--block button--small"]')
+
+    # Подать объявление
+    btn_menu_header_place_an_ad = WebElement(xpath='//button[@class="button button'
+                                                   '--primary button--block button--small"]')
 
 
 
 
+
+
+    # Мобильные приложения
+    btn_menu_footer_mobile_app = WebElement(xpath="//div[@class='footer__app-text']//a[@target='_blank']")
+
+    # Приложение для Android
     btn_menu_footer_android = WebElement(xpath='(//a[@class="app-badge"])[1]')
-    btn_menu_footer_apple = WebElement(xpath='(//a[@class="app-badge"])[2]')
-    btn_menu_footer_huawei = WebElement(xpath='(//a[@class="app-badge"])[3]')
-    btn_menu_footer_star_one = WebElement(xpath='(//button[@class="rating__star"])[1]')
-    btn_menu_footer_star_two = WebElement(xpath='(//button[@class="rating__star"])[2]')
-    btn_menu_footer_star_three = WebElement(xpath='(//button[@class="rating__star"])[3]')
-    btn_menu_footer_star_four = WebElement(xpath='(//button[@class="rating__star"])[4]')
-    btn_menu_footer_star_five = WebElement(xpath='(//button[@class="rating__star"])[5]')
 
+    # Приложение для Android - Текст
+
+    text_menu_footer_android = WebElement(xpath='(//a[@class="app-badge"]//span[@class="app-badge__label"])[1]')
+    text_menu_footer_androids = WebElement(xpath='(//a[@class="app-badge"]//span[@class="app-badge__platform"])[1]')
+
+    # Приложение для iPhone
+    btn_menu_footer_apple = WebElement(xpath='(//a[@class="app-badge"])[2]')
+
+    # Приложение для Huawei
+    btn_menu_footer_huawei = WebElement(xpath='(//a[@class="app-badge"])[3]')
+
+    # Звезды - Рейтинг
+    # btn_menu_footer_star_one = WebElement(xpath='(//button[@class="rating__star"])[1]')
+    # btn_menu_footer_star_two = WebElement(xpath='(//button[@class="rating__star"])[2]')
+    # btn_menu_footer_star_three = WebElement(xpath='(//button[@class="rating__star"])[3]')
+    # btn_menu_footer_star_four = WebElement(xpath='(//button[@class="rating__star"])[4]')
+    # btn_menu_footer_star_five = WebElement(xpath='(//button[@class="rating__star"])[5]')
+
+    # Задать вопрос
     btn_menu_footer_ask_a_question = WebElement(xpath='(//a[@class="footer__link"])[1]')
+
+    # Правила подачи
     btn_menu_footer_submission_rules = WebElement(xpath='(//a[@class="footer__link"])[2]')
 
+    # Реклама на av.by
     btn_menu_footer_advertising_on = WebElement(xpath='(//a[@class="footer__link"])[3]')
+
+    # Pro-аккаунты
     btn_menu_footer_pro_accounts = WebElement(xpath='(//a[@class="footer__link"])[4]')
+
+    # Спецпроекты
     btn_menu_footer_special_projects = WebElement(xpath='(//a[@class="footer__link"])[5]')
+
+    # Медийная реклама
     btn_menu_footer_display_advertising = WebElement(xpath='(//a[@class="footer__link"])[6]')
+
+    # О проекте
     btn_menu_footer_about_the_project = WebElement(xpath='(//a[@class="footer__link"])[7]')
 
+    # Проверка VIN
+    btn_menu_footer_vin_check = WebElement(xpath='(//a[@class="footer__link"])[8]')
 
+    # Оценка стоимости
+    btn_menu_footer_cost_estimate = WebElement(xpath='(//a[@class="footer__link"])[9]')
 
+    # Финансы
+    btn_menu_footer_finance = WebElement(xpath='(//a[@class="footer__link"])[10]')
+
+    # Услуги
+    btn_menu_footer_services = WebElement(xpath='(//a[@class="footer__link"])[11]')
+
+    # Рассрочка
+    btn_menu_footer_installment_plan = WebElement(xpath='(//a[@class="footer__link"])[12]')
+
+    # Каталог модификаций
+    btn_menu_footer_modification_catalog = WebElement(xpath='(//a[@class="footer__link"])[13]')
+
+    # Таможенный калькулятор
+    btn_menu_footer_customs_calculator = WebElement(xpath='(//a[@class="footer__link"])[14]')
+
+    # Печать объявления
+    btn_menu_footer_print_advertisement = WebElement(xpath='(//a[@class="footer__link"])[15]')
+
+    # Авто журнал
+    btn_menu_footer_auto_magazine = WebElement(xpath='(//a[@class="footer__link"])[16]')
+
+    # Авто с пробегом
+    btn_menu_footer_used_car = WebElement(xpath='(//a[@class="footer__link"])[17]')
+
+    # Электромобили
+    btn_menu_footer_electric_cars = WebElement(xpath='(//a[@class="footer__link"])[18]')
+
+    # Новые авто
+    btn_menu_footer_new_cars = WebElement(xpath='(//a[@class="footer__link"])[19]')
+
+    # Мототехника
+    btn_menu_footer_motor_vehicles = WebElement(xpath='(//a[@class="footer__link"])[20]')
+
+    # Сельхозтехника
+    btn_menu_footer_agricultural_machinery = WebElement(xpath='(//a[@class="footer__link"])[21]')
+
+    # Грузовики
+    btn_menu_footer_trucks = WebElement(xpath='(//a[@class="footer__link"])[22]')
+
+    # Прицепы
+    btn_menu_footer_trailers = WebElement(xpath='(//a[@class="footer__link"])[23]')
+
+    # Водный транспорт
+    btn_menu_footer_water_transport = WebElement(xpath='(//a[@class="footer__link"])[24]')
+
+    # Спецтехника
+    btn_menu_footer_special_equipment = WebElement(xpath='(//a[@class="footer__link"])[25]')
+
+    # Автобусы
+    btn_menu_footer_buses = WebElement(xpath='(//a[@class="footer__link"])[26]')
+
+    # Запчасти и авто товары
+    btn_menu_footer_spare_and_products = WebElement(xpath='(//a[@class="footer__link"])[27]')
+
+    # Шины и диски
+    btn_menu_footer_tires_and_wheels = WebElement(xpath='(//a[@class="footer__link"])[28]')
+
+    # YouTube
+    btn_menu_footer_youtube = WebElement(xpath='(//a[@class="footer__link"])[29]')
+
+    # Instagram
+    btn_menu_footer_instagram = WebElement(xpath='(//a[@class="footer__link"])[30]')
+
+    # Telegram
+    btn_menu_footer_telegram = WebElement(xpath='(//a[@class="footer__link"])[31]')
+
+    # TikTok
+    btn_menu_footer_tiktok = WebElement(xpath='(//a[@class="footer__link"])[32]')
+
+    # ВКонтакте
+    btn_menu_footer_vk = WebElement(xpath='(//a[@class="footer__link"])[33]')
+
+    # Facebook
+    btn_menu_footer_facebook = WebElement(xpath='(//a[@class="footer__link"])[34]')
+
+    # Twitter
+    btn_menu_footer_twitter = WebElement(xpath='(//a[@class="footer__link"])[35]')
+
+    # Одноклассники
+    btn_menu_footer_classmates = WebElement(xpath='(//a[@class="footer__link"])[36]')
+
+    # Пользовательское соглашение
+    btn_menu_footer_users_agreement = WebElement(xpath='//div[@class="footer__links"]//a[1]')
+
+    # Правила обработки персональных данных
+    btn_menu_footer_personal_data = WebElement(xpath='//div[@class="footer__links"]//a[2]')
+
+    # Политика использования cookie-файлов
+    btn_menu_footer_cookie_files = WebElement(xpath='//div[@class="footer__links"]//button')
 
 
--- tests/test_main_page_avby.py
+++ tests/test_main_page_avby.py
@@ -11,7 +11,6 @@ def test_headers_main(web_browser):
     """Этот тест проверяет кликабельность, наличие элементов, провописание """
 
     page = MainPage(web_browser)
-
     elements = [(page.btn_menu_header_announcement, 'Объявления', 'https://cars.av.by/'),
                 (page.btn_menu_header_services, 'Сервисы', 'https://av.by/vin'),
                 (page.btn_menu_header_magazine, 'Журнал', 'https://av.by/news'),
@@ -36,3 +35,86 @@ def test_headers_main(web_browser):
 
         with allure.step("Тест проверки на правильный адрес кнопки"):
             check.equal(element.get_attribute('href'), url_elements)
+
+
+
+
+@allure.story('Тест главной страницы "av.by"')
+@allure.feature('Проверка футера')
+def test_footers_main(web_browser):
+    """Этот тест проверяет кликабельность, наличие элементов, провописание """
+
+    page = MainPage(web_browser)
+
+    elements = [(page.btn_menu_footer_ask_a_question, 'Задать вопрос', 'https://av.by/support'),
+                (page.btn_menu_footer_submission_rules, 'Правила подачи', 'https://av.by/pages/rules'),
+                (page.btn_menu_footer_advertising_on, 'Реклама на av.by', 'https://av.by/pages/adv'),
+                (page.btn_menu_footer_pro_accounts, 'Pro-аккаунты', 'https://av.by/pages/pro-accounts'),
+                (page.btn_menu_footer_special_projects, 'Спецпроекты', 'https://av.by/pages/pr_instruments'),
+                (page.btn_menu_footer_display_advertising, 'Медийная реклама', 'https://av.by/pages/index_adv'),
+                (page.btn_menu_footer_about_the_project, 'О проекте', 'https://av.by/pages/about'),
+                (page.btn_menu_footer_vin_check, 'Проверка VIN', 'https://av.by/vin'),
+                (page.btn_menu_footer_cost_estimate, 'Оценка стоимости', 'https://av.by/ocenka-avto'),
+                (page.btn_menu_footer_finance, 'Финансы', 'https://av.by/finance'),
+                (page.btn_menu_footer_services, 'Услуги', 'https://av.by/company'),
+                (page.btn_menu_footer_installment_plan, 'Рассрочка', 'https://av.by/rassrochka'),
+                (page.btn_menu_footer_modification_catalog, 'Каталог модификаций', 'https://av.by/catalog'),
+                (page.btn_menu_footer_customs_calculator, 'Таможенный калькулятор', 'https://av.by/customs-calculator'),
+                (page.btn_menu_footer_print_advertisement, 'Печать объявления', 'https://av.by/print'),
+                (page.btn_menu_footer_auto_magazine, 'Автожурнал', 'https://av.by/news'),
+                (page.btn_menu_footer_used_car, 'Авто с пробегом', 'https://cars.av.by/'),
+                (page.btn_menu_footer_electric_cars, 'Электромобили', 'https://cars.av.by/electrocars'),
+                (page.btn_menu_footer_new_cars, 'Новые авто', 'https://salon.av.by/'),
+                (page.btn_menu_footer_motor_vehicles, 'Мототехника', 'https://moto.av.by/'),
+                (page.btn_menu_footer_agricultural_machinery, 'Сельхозтехника', 'https://agro.av.by/'),
+                (page.btn_menu_footer_trucks, 'Грузовики', 'https://truck.av.by/'),
+                (page.btn_menu_footer_trailers, 'Прицепы', 'https://trailer.av.by/'),
+                (page.btn_menu_footer_water_transport, 'Водный транспорт', 'https://boat.av.by/'),
+                (page.btn_menu_footer_special_equipment, 'Спецтехника', 'https://spec.av.by/'),
+                (page.btn_menu_footer_buses, 'Автобусы', 'https://bus.av.by/'),
+                (page.btn_menu_footer_spare_and_products, 'Запчасти и автотовары', 'https://parts.av.by/'),
+                (page.btn_menu_footer_tires_and_wheels, 'Шины и диски', 'https://koleso.av.by/'),
+                (page.btn_menu_footer_users_agreement, 'Пользовательское соглашение',
+                 'https://av.by/pages/user_agreement'),
+                (page.btn_menu_footer_personal_data, 'Правила обработки персональных данных',
+                 'https://av.by/privacy_policy'),
+                ]
+
+    # elements_app = [
+    #             (page.btn_menu_footer_mobile_app, 'мобильные приложения', 'https://av.by/pages/app'),
+    #             (page.btn_menu_footer_android, 'Приложение для Android', 'https://play.google.com/store/apps/details?id='
+    #                                                              'by.av.client&utm_source=av_by&utm_medium=footer&'
+    #                                                              'utm_campaign=playmarket_icon'),
+    #             (page.btn_menu_footer_apple, 'Приложение для iPhone', 'https://itunes.apple.com/by/app/av-by'
+    #                                                           '-продажа-автомобилей/id1020154366?mt=8'),
+    #             (page.btn_menu_footer_huawei, 'Приложение для Huawei', 'https://appgallery.huawei.com/'
+    #                                                                    '#/app/C101652541'),
+    #             (page.btn_menu_footer_youtube, 'YouTube', 'https://www.youtube.com/c/etoavby'),
+    #             (page.btn_menu_footer_instagram, 'Instagram', 'https://www.instagram.com/insta_avby/'),
+    #             (page.btn_menu_footer_telegram, 'Telegram', 'https://t.me/avbynews'),
+    #             (page.btn_menu_footer_tiktok, 'TikTok', 'https://www.tiktok.com/@av.by'),
+    #             (page.btn_menu_footer_vk, 'ВКонтакте', 'https://vk.com/newsavby'),
+    #             (page.btn_menu_footer_facebook, 'Facebook', 'https://www.facebook.com/belarus.auto'),
+    #             (page.btn_menu_footer_twitter, 'Twitter', 'https://twitter.com/avby_bel'),
+    #             (page.btn_menu_footer_classmates, 'Одноклассники', 'https://ok.ru/av.by')
+    #             ]
+
+    for element, text_element, url_elements in elements:
+        with allure.step('Тест проверки правильного URL при переходе'):
+            element.click()
+            time.sleep(5)
+            check.equal(page.get_current_url(), url_elements)
+
+        with allure.step("Проверка на наличие на экране элементов"):
+            check.is_true(element.is_visible())
+
+        with allure.step("Проверка орфографии"):
+            check.equal(element.get_text(), text_element)
+
+        with allure.step("Тест проверки на правильный адрес кнопки"):
+            check.equal(element.get_attribute('href'), url_elements)
+
+
+
+
+
